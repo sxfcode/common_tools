59f9c2899723240465b62f1bf59ff27015d49611
suxiaofei <suxiaofei.tech@gmail.com>
2014-05-02 14:16:04 +0800

    设计模式-调整示例类名称为ZExample

--- src/designpattern/create/prototype/ZExample.java
+++ src/designpattern/create/prototype/ZExample.java
@@ -14,7 +14,8 @@ import designpattern.create.prototype.lightclone.LightCustomObj;
 import designpattern.create.prototype.lightclone.LightParentObj;
 
 /**
- * The Class PrototypeObj. 原型模式 ==================================
+ * 
+ * 原型模式 ==================================
  * 用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新的对象。
  * Prototype原型模式是一种创建型设计模式,Prototype模式允许一个对象再创建另外一个可定制的对象， 根本无需知道任何如何创建的细节,
  * 工作原理是:通过将一个原型对象传给那个要发动创建的对象 ，这个要发动创建的对象通过请求原型对象拷贝它们自己来实施创建。
