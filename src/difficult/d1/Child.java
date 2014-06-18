5e16e582dbfc1626e60e7c8e4fc50a049461bbf1
suxiaofei <suxiaofei.tech@gmail.com>
2014-06-18 14:24:25 +0800

    常见难点理解

---
+++ src/difficult/d1/Child.java
@@ -0,0 +1,17 @@
+package difficult.d1;
+
+public class Child extends Parent {
+
+	private int x = 200;
+	public Child(int x) {
+		System.out.println("child construct");
+		this.x = x;
+		this.print();
+	}
+
+	@Override
+	void print() {
+		System.out.println("chid print x ="+x);
+	}
+
+}
