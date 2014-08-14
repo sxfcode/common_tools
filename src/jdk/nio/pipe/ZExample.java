854ff367f8a08d8c033367f62a7415b2a9c2839f
suxiaofei <suxiaofei.tech@gmail.com>
2014-08-14 14:16:10 +0800

    nio

--- src/jdk/nio/pipe/ZExample.java
+++ src/jdk/nio/pipe/ZExample.java
@@ -3,6 +3,10 @@ package jdk.nio.pipe;
 import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The Class ZExample.
@@ -19,8 +23,9 @@ public class ZExample {
 	 * The main method.
 	 *
 	 * @param args the arguments
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		PipedReader reader = new PipedReader();
 		PipedWriter writer = new PipedWriter();
 		try {
@@ -32,6 +37,20 @@ public class ZExample {
 		WriteThread wt = new WriteThread(writer);
 		rt.start();
 		wt.start();
+		
+		HashMap<String,String> m1 = new HashMap<String,String>();
+		m1.put("n1", "number1");
+		m1.put("n2", "number2");
+		ArrayList<String> al =new ArrayList<String>();
+		al.add("AL01");
+		al.add("AL02");
+		Set<String> st = new HashSet<String>();
+		st.add("set01");
+		st.add("set02");
+		String temp = "temp01";
+		System.out.println(temp);
+		rt.join();
+		
 	}
 
 }
