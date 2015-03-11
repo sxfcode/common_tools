704d8de3cb697211d5e1cff39d636f365e2865de
suxiaofei <suxiaofei.tech@gmail.com>
2015-03-11 16:35:24 +0800

    排序:简化插入排序算法，参考jdk,Arrays.mergeSort

--- src/core/algorithm/sort/insert/InsertSort.java
+++ src/core/algorithm/sort/insert/InsertSort.java
@@ -50,96 +50,45 @@ import core.algorithm.sort.Sort;
 public class InsertSort implements Sort{
 	
 
-	@Override
-	public int[] sort(int[] sourceArray) {
-		return InsertSort.insertSort(sourceArray);
-	}
-	
-	
+
 	/**
 	 * 插入排序
 	 * (节省空间的算法,在同一数组上进行排序)
 	 * 空间复杂度 O(n)
-	 * 时间复杂度 比较
-	 * 
-	 * @param sourceArray
-	 * @return
+	 * 时间复杂度 
 	 */
-	public static int[] insertSort(int[] sourceArray){
-		// step 1:遍历源数组，以i元素为边界，将源数组分成两部分,前半部分(不包含i)和后半部分(包含i)。前半部分为已排序数组，后半部分为未排序数
-		for(int i=0;i<sourceArray.length;i++){
-			int current = sourceArray[i];
-			// step 2:将i元素与前半部分的最后一个元素比较。第i个元素跟前i-1个元素进行比较，并插入到合适的位置，前i-1个元素认为已经排序完毕
-			for(int j=i-1;j>=0;j--){
-				// step 3:若i元素大于当前元素，则将i插入到当前元素后边，否则继续向前比较。
-				if(current>=sourceArray[j]){
-					// 插入并向后移动元素,由于最后元素的索引不方便确认，所以采用向后推进的方式，而不常规的向前推进的方式。
-					// mov1 移动从j+1到i之间的元素
-					int last = current;
-					for(int k=j+1;k<=i;k++){
-						int temp = sourceArray[k];
-						sourceArray[k] = last;
-						last = temp;
-					}
-					// 若不执行break,则k==0的操作会再次执行，导致逻辑错误。
-					break;
-				}else{
-					// 所有元素都比current大的情况，则将其放到数组开头
-					if(j==0){
-						// mov2:移动从0到i之间的元素(注意mov2这里要比mov1多移动一个元素)
-						int last = current;
-						for(int k=j;k<=i;k++){
-							int temp = sourceArray[k];
-							sourceArray[k] = last;
-							last = temp;
-						}
-					}
-				}
-			}
-		}
+	public int[] sort(int[] sourceArray) {
+	     for (int i=0; i<sourceArray.length; i++)
+             for (int j=i; j>0 && sourceArray[j-1]>sourceArray[j]; j--)
+                 swap(sourceArray, j, j-1);
 		return sourceArray;
 	}
 	
 	/**
-	 * 插入排序
-	 * (额外需要2个数组的存储空间，主要用于演示)
-	 * @param sourceArray
-	 * @return
+	 * swap.
+	 *
+	 * @param src comments
+	 * @param i comments
+	 * @param j comments
 	 */
-	public static int[] insertSortShow(int[] sourceArray){
-		if (sourceArray==null||sourceArray.length<=1) {
-			return sourceArray;
+	public static void swap(int[] src,int i,int j){
+		int a =src[i];
+		src[i]=src[j];
+		src[j] = a;
+	}
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		int[] sr = new int[]{3,4,2};
+		InsertSort is = new InsertSort();
+		is.sort(sr);
+		for (int i = 0; i < sr.length; i++) {
+			System.out.println(sr[i]);
 		}
-		// 已排序数组(按从小到大的顺序排,其length等于源数组)
-		int[] sortedArray = new int[sourceArray.length];
-		// 未排序数组(其length等于源数组)
-		int[] notSortedArray = new int[sourceArray.length];
-		System.arraycopy(sourceArray, 0, notSortedArray, 0, sourceArray.length);
-		sortedArray[0]= sourceArray[0];
-		for (int i = 1; i < notSortedArray.length; i++) {
-			int current = notSortedArray[i];
-			// 从已排序数组的最末位置开始比较,
-			// 若当前元素(未排序)，大于已排序数组末尾的元素last，则将其插入到排序数组，
-			// 否则，与已排序数组的前一个元素(last-1)比较，若其大于(last-1),则将其插入到last位置，否则继续迁移进行比较，直到第一个元素。
-			for (int j = i-1; j < sortedArray.length && j>=0; j--) {
-				if(current>=sortedArray[j]){
-					// 移动后边(i-j)个的元素
-					for (int k = i-1; k < sortedArray.length && k > j ; k--) {
-						sortedArray[k+1] = sortedArray[k];
-					}
-					sortedArray[j+1] = current;
-					break;
-				}else{
-					if(j==0){
-						// 移动后边(i-j)个的元素,将当前元素插入到数组头部。小于0时，第j个元素也要后移，同同时将current赋给j
-						for (int k = i-1; k < sortedArray.length && k >= j ; k--) {
-							sortedArray[k+1] = sortedArray[k];
-						}
-						sortedArray[j] = current;
-					}
-				}
-			}
-		}
-		return sortedArray;
+		
 	}
 }
